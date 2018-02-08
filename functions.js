b3638d01eeb525d3e40ac879e18ffba2b4dc61a8
Dumitru Jr Campan <dumitru.campan@gmail.com>
2018-02-08 12:34:42 -0600

    Makeover

---
+++ functions.js
@@ -0,0 +1,169 @@
+function getUnits(word) {
+    if (word.length == 0) return 0;
+
+    switch (word) {
+        case "one":
+            return 1;
+        case "two":
+            return 2;
+        case "three":
+            return 3;
+        case "four":
+            return 4;
+        case "five":
+            return 5;
+        case "six":
+            return 6;
+        case "seven":
+            return 7;
+        case "eight":
+            return 8;
+        case "nine":
+            return 9;
+        case "ten":
+            return 10;
+        case "eleven":
+            return 11;
+        case "twelve":
+            return 12;
+        case "thirteen":
+            return 13;
+        case "fourteen":
+            return 14;
+        case "fifteen":
+            return 15;
+        case "sixteen":
+            return 16;
+        case "seventeen":
+            return 17;
+        case "eighteen":
+            return 18;
+        case "nineteen":
+            return 19;
+        case "twenty":
+            return 20;
+        case "thirty":
+            return 30;
+        case "forty":
+            return 40;
+        case "fifty":
+            return 50;
+        case "sixty":
+            return 60;
+        case "seventy":
+            return 70;
+        case "eighty":
+            return 80;
+        case "ninety":
+            return 90;
+        case "hundred":
+            return 100;
+        case "thousand":
+            return 1000;
+    }
+
+    var index, preWord, postWord;
+
+    index = word.search(" ");
+    preWord = word.substr(0, index);
+    postWord = word.substr(preWord.length + 1);
+
+    if (index >= 0) {
+        return getUnits(preWord) * getUnits(postWord);
+    }
+
+    index = word.search("-");
+    preWord = word.substr(0, index);
+    postWord = word.substr(preWord.length + 1);
+
+    return (index >= 0) ? getUnits(preWord) + getUnits(postWord) : 0;
+}
+
+function getMixedUnits(words) {
+    var spaceIndex = words.search(" ");
+    var preWord = words.substr(0, spaceIndex);
+    var postWord = words.substr(preWord.length + 1);
+
+    return (spaceIndex >= 0) ? getUnits(preWord) * getUnits(postWord) : getUnits(words);
+}
+
+function getNumber(words) {
+    var andIndex = words.search(" and ");
+    var mixedWords = words.substr(0, andIndex);
+    var unitWords = words.substr(mixedWords.length + 5);
+
+    return (andIndex >= 0) ? getMixedUnits(mixedWords) + getUnits(unitWords) : getUnits(words);
+}
+
+function calculate(sentence) {
+    var first = '',
+        second = '';
+
+    var minus = sentence.search(" minus ");
+    var plus = sentence.search(" plus ");
+    var times = sentence.search(" times ");
+
+    if (plus >= 0) {
+        first = sentence.substr(0, plus);
+        second = sentence.substr(plus + 6);
+
+        return calculate(first) + calculate(second);
+    }
+
+    if (minus >= 0) {
+        first = sentence.substr(0, minus);
+        second = sentence.substr(minus + 7);
+
+        var minusIndex = second.search(" minus ");
+        var third = second.substr(0, minusIndex);
+        var fourth = second.substr(minusIndex + 7);
+
+        if (minusIndex >= 0) {
+            return calculate(first) - (calculate(third) + calculate(fourth));
+        }
+
+        return calculate(first) - calculate(second);
+    }
+
+    if (times >= 0) {
+        first = sentence.substr(0, times);
+        second = sentence.substr(times + 7);
+
+        return calculate(first) * calculate(second);
+    }
+
+    return getNumber(sentence);
+}
+
+function processSentence(sentence) {
+    var first = '',
+        second = '';
+
+    var minus = sentence.search(" minus ");
+    var plus = sentence.search(" plus ");
+    var times = sentence.search(" times ");
+
+    if (plus >= 0) {
+        first = sentence.substr(0, plus);
+        second = sentence.substr(plus + 6);
+
+        return processSentence(first) + ' + ' + processSentence(second);
+    }
+
+    if (minus >= 0) {
+        first = sentence.substr(0, minus);
+        second = sentence.substr(minus + 7);
+
+        return processSentence(first) + ' - ' + processSentence(second);
+    }
+
+    if (times >= 0) {
+        first = sentence.substr(0, times);
+        second = sentence.substr(times + 7);
+
+        return processSentence(first) + ' * ' + processSentence(second);
+    }
+
+    var number = getNumber(sentence);
+    return " " + number;
+}
--- script.js
+++ script.js
@@ -1,232 +1,46 @@
 $(function(){
-	
-	function getUnits(word)
-	{
-		if(word.length == 0) return 0;
-		
-		switch(word)
-		{
-			case "one":       return 1;
-			case "two":       return 2;
-			case "three":     return 3;
-			case "four":      return 4;
-			case "five":      return 5;
-			case "six":       return 6;
-			case "seven":     return 7;
-			case "eight":     return 8;
-			case "nine":      return 9;
-			case "ten":       return 10;
-			case "eleven":    return 11;
-			case "twelve":    return 12;
-			case "thirteen":  return 13;
-			case "fourteen":  return 14;
-			case "fifteen":   return 15;
-			case "sixteen":   return 16;
-			case "seventeen": return 17;
-			case "eighteen":  return 18;
-			case "nineteen":  return 19;
-			case "twenty":    return 20;
-			case "thirty":    return 30;
-			case "forty":     return 40;
-			case "fifty":     return 50;
-			case "sixty":     return 60;
-			case "seventy":   return 70;
-			case "eighty":    return 80;
-			case "ninety":    return 90;
-			case "hundred":   return 100;
-			case "thousand":  return 1000;
+	function errorHandler(error){
+		console.log('Something went wrong while trying to retrieve file!', error);
+	}
+
+	function buildResultsTable(results){
+		var output = '<table class="pure-table pure-table-bordered">';
+		output += '<thead><tr><th colspan="3">Results</th></tr></thead><tbody>';
+
+		results.forEach(function(result){
+			output += '<tr><td>' + result.question + '</td>';
+			output += '<td>' + result.formula + '</td>';
+			output += '<td>' + result.answer + '</td></tr>';
+		});
+
+		output += '</tbody></table>';
+		return output;
+	}
+
+	function processQuestion(question){
+		var sentence = question.substr(8).substr(0, question.length -9);
+		var formula = processSentence(sentence);
+		var answer = calculate(sentence);
+
+		return { 
+			question: question, 
+			formula: formula, 
+			answer: answer 
 		}
-		
-		var index, preWord, postWord;
-		
-		index = word.search(" ");
-		preWord = word.substr(0, index);
-		postWord = word.substr(preWord.length + 1);
-		
-		if(index >= 0){
-			return getUnits(preWord) * getUnits(postWord);
-		}
-		
-		index = word.search("-");
-		preWord = word.substr(0, index);
-		postWord = word.substr(preWord.length + 1);
-		
-		return (index >= 0) ? getUnits(preWord) + getUnits(postWord) : 0;
 	}
 	
-	function getMixedUnits(words)
-	{
-		var spaceIndex = words.search(" ");
-		var preWord = words.substr(0, spaceIndex);
-		var postWord = words.substr(preWord.length + 1);
-		
-		return (spaceIndex >= 0) ? getUnits(preWord) * getUnits(postWord) : getUnits(words);
-	}
-	
-	function getNumber(words)
-	{
-		var andIndex = words.search(" and ");
-		var mixedWords = words.substr(0, andIndex);
-		var unitWords = words.substr(mixedWords.length + 5);
-		
-		return (andIndex >= 0) ? getMixedUnits(mixedWords) + getUnits(unitWords) : getUnits(words);
-	}
-	
-	function calculate(sentence)
-	{
-		var first = '', second = '';
-		
-		var minus = sentence.search(" minus ");
-		var plus = sentence.search(" plus ");
-		var times = sentence.search(" times ");
-		
-		if(plus >= 0)
-		{
-			first = sentence.substr(0, plus);
-			second = sentence.substr(plus + 6);
-			
-			return calculate(first) + calculate(second);
-		}	
-		
-		if(minus >= 0)
-		{
-			first = sentence.substr(0, minus);
-			second = sentence.substr(minus + 7);
-			
-			var minusIndex = second.search(" minus ");
-			var third = second.substr(0, minusIndex);
-			var fourth = second.substr(minusIndex + 7);
-			
-			if(minusIndex >= 0){
-				return calculate(first) - (calculate(third) + calculate(fourth));
-			}
-			
-			return calculate(first) - calculate(second);
-		}
-		
-		if(times >= 0)
-		{
-			first = sentence.substr(0, times);
-			second = sentence.substr(times + 7);
-		
-			return calculate(first) * calculate(second);
-		}
-		
-		return getNumber(sentence);
-	}
-	
-	function processSentence(sentence)
-	{
-		var first = '', second = '';
-		
-		var minus = sentence.search(" minus ");
-		var plus = sentence.search(" plus ");
-		var times = sentence.search(" times ");
-		
-		if(plus >= 0)
-		{
-			first = sentence.substr(0, plus);
-			second = sentence.substr(plus + 6);
-			
-			return processSentence(first) + ' + ' + processSentence(second);
-		}	
-		
-		if(minus >= 0)
-		{
-			first = sentence.substr(0, minus);
-			second = sentence.substr(minus + 7);
-			
-			return processSentence(first) + ' - ' + processSentence(second);
-		}
-		
-		if(times >= 0)
-		{
-			first = sentence.substr(0, times);
-			second = sentence.substr(times + 7);
-		
-			return processSentence(first) + ' * ' + processSentence(second);
-		}
-		
-		var number = getNumber(sentence);
-		return " " + number;
-	}
-	
-	function processQuestions(questions)
-	{
+	function processQuestions(questions){
 		var results = [];
-		var len = questions.length;
-		
-		for(var i = 0; i < len; ++i)
-		{
-			var sentence = questions[i].substr(8).substr(0, questions[i].length -9);
-			results.push(calculate(sentence));
-		}
-		
-		return results;
-	}
-	
-	function getFunction(result){
-		var questions = result["Questions"];
-		var results = processQuestions(questions);
-		
-		var display = $('#display'), output = '';
-		
-		for(var i = 0; i < questions.length; ++i)
-		{
-			var sentence = questions[i].substr(8).substr(0, questions[i].length -9).toLowerCase();
-			//if(sentence.search(" minus ") >= 0)
-				output += "Q[" + (i + 1) + "]: " + sentence; 
-				output += '<span class="floater">' + processSentence(sentence) + ' = ' + results[i] + '</span><br/>';
-		}
-		
-		display.html(output);
-	}
-	
-	$('#getButton').on('click', function(e){
-		e.preventDefault();
-		
-		$.ajax({
-			type: 'GET',
-			url: 'http://multiplysixbynine.com:8000/v1/questions',
-			crossDomain: true,
-			dataType: 'json',
-			jsonpCallback: 'callback',
-			success: getFunction
+		questions.forEach(function(question){
+			results.push(processQuestion(question));
 		});
-	});
-	
-	$('#submitButton').on('click', function(e){
-		e.preventDefault();
-		
-		$.ajax({
-			type: 'GET',
-			url: 'http://multiplysixbynine.com:8000/v1/questions',
-			crossDomain: true,
-			dataType: 'json',
-			jsonpCallback: 'callback',
-			success: function(result){
-				var uri = 'github.com/DOOMitru'
-				var key = result["Key"];
-				var questions = result["Questions"];
-				var results = processQuestions(questions);
-				
-				$.ajax({
-					type: 'POST',
-					url: 'http://multiplysixbynine.com:8000/v1/answers',
-					crossDomain: true,
-					dataType: 'json',
-					data: JSON.stringify({
-						Url: uri,
-						Key: key,
-						Answers: results
-					}),
-					success: function(result) {
-						$('#display').html(result["Message"] || result["Time"]);
-						console.log(result)
-					}
-				});
-				
-			}
-		});
+
+		var output = buildResultsTable(results);
+		$('#display').html(output);
+	}
+
+	$('#processButton').on('click', function(e){
+		$('#display').html('Processing...');
+		$.getJSON('questions.json').then(processQuestions, errorHandler);
 	});
 });
