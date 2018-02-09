db9b60bd2ff1f37db5f6281bb9066b464f6eab81
Dumitru Jr Campan <dumitru.campan@gmail.com>
2018-02-09 11:12:21 -0600

    Rewrite using vanilla javascript

--- script.js
+++ script.js
@@ -1,46 +1,65 @@
-$(function(){
-	function errorHandler(error){
-		console.log('Something went wrong while trying to retrieve file!', error);
-	}
+function getFile(url, callback){
+	var xmlHttp = new XMLHttpRequest();
 
-	function buildResultsTable(results){
-		var output = '<table class="pure-table pure-table-bordered">';
-		output += '<thead><tr><th colspan="3">Results</th></tr></thead><tbody>';
-
-		results.forEach(function(result){
-			output += '<tr><td>' + result.question + '</td>';
-			output += '<td>' + result.formula + '</td>';
-			output += '<td>' + result.answer + '</td></tr>';
-		});
-
-		output += '</tbody></table>';
-		return output;
-	}
-
-	function processQuestion(question){
-		var sentence = question.substr(8).substr(0, question.length -9);
-		var formula = processSentence(sentence);
-		var answer = calculate(sentence);
-
-		return { 
-			question: question, 
-			formula: formula, 
-			answer: answer 
+	xmlHttp.onreadystatechange = function(){
+		if(xmlHttp.readyState === 4 && xmlHttp.status === 200){
+            try {
+				var data = JSON.parse(xmlHttp.responseText);
+				callback(data);
+            } catch(err) {
+				console.error(err.message, xmlHttp);
+				return;
+			}
 		}
-	}
-	
-	function processQuestions(questions){
-		var results = [];
-		questions.forEach(function(question){
-			results.push(processQuestion(question));
-		});
+	};
 
-		var output = buildResultsTable(results);
-		$('#display').html(output);
-	}
+	xmlHttp.open('GET', url, true);
+	xmlHttp.send();
+}
 
-	$('#processButton').on('click', function(e){
-		$('#display').html('Processing...');
-		$.getJSON('questions.json').then(processQuestions, errorHandler);
+function buildResultsTable(results){
+	var output = '<table class="pure-table pure-table-bordered">';
+	output += '<thead><tr><th colspan="3">Results</th></tr></thead><tbody>';
+
+	results.forEach(function(result){
+		output += '<tr><td>' + result.question + '</td>';
+		output += '<td>' + result.formula + '</td>';
+		output += '<td>' + result.answer + '</td></tr>';
 	});
+
+	output += '</tbody></table>';
+	return output;
+}
+
+function processQuestion(question){
+	var sentence = question.substr(8).substr(0, question.length -9);
+	var formula = processSentence(sentence);
+	var answer = calculate(sentence);
+
+	return { 
+		question: question, 
+		formula: formula, 
+		answer: answer 
+	}
+}
+
+function processQuestions(questions){
+	var results = [];
+
+	questions.forEach(function(question){
+		results.push(processQuestion(question));
+	});
+
+	var output = buildResultsTable(results);
+	document.getElementById('display').innerHTML = output;
+}
+
+function clickHandler(){
+	document.getElementById('display').innerHTML = 'Processing...';
+	getFile('questions.json', processQuestions);
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+	var button = document.getElementById('processButton');
+	button.addEventListener('click', clickHandler);
 });
